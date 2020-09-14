4581b32e3267e79b90de1fd57b3e19f23be0a473
dani <dani.zaghian@gmail.com>
2020-09-14 12:07:40 -0700

    adds recipe model methods

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,22 @@
 class Recipe < ApplicationRecord
+  def ingredients_list
+    ingredients.split(", ").map { |ingredient| ingredient.capitalize }
+  end
+
+  def directions_list
+    directions.split(", ").map { |ingredient| ingredient.capitalize }
+  end
+
+  def friendly_created_at
+    created_at.strftime("%b %d, %Y")
+  end
+
+  def friendly_prep_time
+    hours = prep_time / 60
+    minutes = prep_time % 60
+    result = ""
+    result += "#{hours} hours " if hours > 0
+    result += "#{minutes} minutes" if minutes > 0
+    result
+  end
 end
